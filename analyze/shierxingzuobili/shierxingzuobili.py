1133754fbb99c62ee8710d9012b80edf7255d668
K <k@KdeMacBook-Pro.local>
2021-02-04 22:11:37 +0800

    shanbei_python

---
+++ analyze/shierxingzuobili/shierxingzuobili.py
@@ -0,0 +1,16 @@
+import pygal
+pie_chart = pygal.Pie()
+pie_chart.title = '中国各星座人口比例（%)'
+pie_chart.add('天蝎', 15.74)
+pie_chart.add('射手', 9.86)
+pie_chart.add('双子', 9.97)
+pie_chart.add('金牛', 9.31)
+pie_chart.add('巨蟹', 9.27)
+pie_chart.add('白羊', 8.66)
+pie_chart.add('水瓶', 6.85)
+pie_chart.add('狮子', 6.70)
+pie_chart.add('双鱼', 6.40)
+pie_chart.add('天秤', 6.31)
+pie_chart.add('摩羯', 5.75)
+pie_chart.add('处女',5.36 )
+pie_chart.render()
---
+++ littlegame/tuxiangshibie/tuxiangshibie.py
@@ -0,0 +1,5 @@
+# from shanbay_ocr import WordOcr
+
+# wordOcr = WordOcr()
+# img = wordOcr.selectImg()
+# print(wordOcr.ocr(img))
